d5643c3ccdcea446ad817b46cbe96f59d025b50a
Eric Socolofsky <eric@transmote.com>
2017-04-10 23:24:14 -0700

    publish: More slides, flesh out motion section
    generated from commit c1ccbfafde40edc08db18969e502c6edd0e2c9b0

---
+++ ovc2017/js/app.js
@@ -0,0 +1,44 @@
+(function () {
+
+	// monkeypatch layout() to enable calculating slide size without backgroundCaption
+	var revealLayout = Reveal.layout;
+	Reveal.layout = function () {
+		slides = Array.prototype.slice.call( document.querySelectorAll( '.reveal .slides section' ) );
+
+		for( var i = 0, len = slides.length; i < len; i++ ) {
+			var slide = slides[ i ],
+				backgroundCaption = slide.querySelector('.background-caption');
+			if (backgroundCaption) backgroundCaption.style.display = 'none';
+		}
+
+		revealLayout();
+
+		for( var i = 0, len = slides.length; i < len; i++ ) {
+			var slide = slides[ i ],
+				backgroundCaption = slide.querySelector('.background-caption');
+			if (backgroundCaption) backgroundCaption.style.display = 'block';
+		}
+	}
+
+	Reveal.addEventListener( 'slidechanged', function( event ) {
+
+		/*
+		// position background caption at bottom of slide
+		var backgroundCaption = event.currentSlide.querySelector('.background-caption');
+		if (backgroundCaption) backgroundCaption.style.top = event.currentSlide.style.top;
+		*/
+
+	});
+
+	// install flowplayer into selected container
+	var container = document.getElementById('flv01');
+	flowplayer(container, {
+		clip: {
+			sources: [{
+				type: 'video/flash',
+				src: 'http://transmote.com/talks/uq-teaser.flv'
+			}]
+		}
+	});
+
+})();
